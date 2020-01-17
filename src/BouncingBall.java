22a4fdecba39a282ffe98ddfe270bbf5719a0e80
Michael Servilla <chico@unm.edu>
2020-01-16 22:32:18 -0700

    Removed unused import, changed <ActionEvent> t <>, and changed Sphere object to Circle object.

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -1,5 +1,4 @@
 import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -9,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -23,20 +23,16 @@ public class BouncingBall extends Application
         Scene scene = new Scene(canvas, 600, 600);
         scene.setFill(Color.CADETBLUE);
 
-        PhongMaterial materialBLUE = new PhongMaterial();
-        materialBLUE.setDiffuseColor(Color.BLUE);
-        Sphere sphere = new Sphere(50, 50);
-        sphere.setMaterial(materialBLUE);
-        sphere.relocate(0,0);
-        canvas.getChildren().add(sphere);
-
+        Circle ball = new Circle(50, Color.BLUE);
+        ball.relocate(0, 0);
+        canvas.getChildren().add(ball);
 
         stage.setTitle("Bouncing Ball");
         stage.setScene(scene);
         stage.show();
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10),
-                new EventHandler<ActionEvent>()
+                new EventHandler<>()
                 {
                     double dx = 7; //Sets step on x (or velocity).
                     double dy = 3; //Sets step on y.
@@ -45,19 +41,19 @@ public class BouncingBall extends Application
                     public void handle(ActionEvent t)
                     {
                         //Moves the ball.
-                        sphere.setLayoutX(sphere.getLayoutX() + dx);
-                        sphere.setLayoutY(sphere.getLayoutY() + dy);
+                        ball.setLayoutX(ball.getLayoutX() + dx);
+                        ball.setLayoutY(ball.getLayoutY() + dy);
 
                         Bounds bounds = canvas.getBoundsInLocal();
 
-                        if(sphere.getLayoutX() <= (bounds.getMinX() + sphere.getRadius()) ||
-                               sphere.getLayoutX() >= (bounds.getMaxX() - sphere.getRadius()))
+                        if(ball.getLayoutX() <= (bounds.getMinX() + ball.getRadius()) ||
+                               ball.getLayoutX() >= (bounds.getMaxX() - ball.getRadius()))
                         {
                             dx = -dx;
                         }
 
-                        if (sphere.getLayoutY() >= (bounds.getMaxY() - sphere.getRadius()) ||
-                                (sphere.getLayoutY() <= (bounds.getMinY() + sphere.getRadius())))
+                        if (ball.getLayoutY() >= (bounds.getMaxY() - ball.getRadius()) ||
+                                (ball.getLayoutY() <= (bounds.getMinY() + ball.getRadius())))
                         {
                             dy = -dy;
                         }
